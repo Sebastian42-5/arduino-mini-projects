caff5e17583ed70b6a5fcdbe2e0788a249636e8e
Sebastian42-5 <sebassotosandrea@gmail.com>
2025-03-18 18:42:14 -0400

    starting little project

---
+++ timer/timer.ino
@@ -0,0 +1,24 @@
+int latch = 9
+int clock = 10
+int data = 8
+int stop_button = 11
+int start_button = 12
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+
+void Display(){
+
+
+
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+
