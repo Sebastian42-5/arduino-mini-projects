08861e2b08bd0b2fb1f877ea266bb94d3e35b9bf
Sebastian42-5 <sebassotosandrea@gmail.com>
2025-03-18 22:25:49 -0400

    finishing for the day

--- timer/timer.ino
+++ timer/timer.ino
@@ -7,17 +7,36 @@ int start_button = 12
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(latch, OUTPUT)
+  pinMode(clock, OUTPUT)
+  pinMode(data, OUTPUT)
+  pinMode(stop_button, INPUT_PULLUP)
+  pinMode(start_button, INPUT_PULLUP)
 
 }
 
 
 void Display(){
+  digitalWrite(latch, LOW)
+  shiftOut(data, clock, MSBFIRST, table[num])
+  digitalWrite(latch, HIGH)
 
 
 
 }
 void loop() {
   // put your main code here, to run repeatedly:
+
+  if (digitalRead(start_button) == LOW)
+  {
+    digitalWrite()
+
+  }
+  if (digitalRead(stop_button) == LOW)
+  {
+    digitalWrite()
+
+  }
 
 }
 
