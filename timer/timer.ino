d9af91feaa2acb5a03f76161fc55d17a23e12630
Sebastian42-5 <sebassotosandrea@gmail.com>
2025-03-22 12:54:35 -0400

    trying to fix

--- timer/timer.ino
+++ timer/timer.ino
@@ -1,56 +1,104 @@
-#include <SevSeg.h>
+// #include <SevSeg.h>
 
-SevSeg sevseg; 
+// SevSeg sevseg; 
 
-// int latch = 9;
-// int clock = 10;
-// int data = 8; 
+int data = 8;
+int latch = 9;
+int clock = 10; 
 
-const int stop_button = 11;
-const int start_button = 12;
+const int start_button = 11;
+const int reset_button = 12;
 
 unsigned long previousMillis = 0;
 const long interval = 1000;
 
-int sec = 0;
-int min = 0;
+int seconds = 0;
+int minutes = 0;
 
 bool isRunning = false; 
 
+const byte digitPatterns[10] = {
+    0b00111111, // 0
+    0b00000110, // 1
+    0b01011011, // 2
+    0b01001111, // 3
+    0b01100110, // 4
+    0b01101101, // 5
+    0b01111101, // 6
+    0b00000111, // 7
+    0b01111111, // 8
+    0b01101111  // 9
+};
+
+const byte digitSelect[4] = {
+    0b00000001, // First digit (rightmost)
+    0b00000010, // Second digit
+    0b00000100, // Third digit
+    0b00001000  // Fourth digit (leftmost)
+};
+
+
 
 void setup() {
+
   // put your setup code here, to run once:
 
-  byte numDigits = 4;
+  // byte numDigits = 4;
 
-  byte numDigits[] = {6, 7, 8, 9};
-  byte digitPins[] = {10, 11, 12, 13}; 
+  // byte shiftRegisterPins[] = {8, 9, 10}; // latch, clock, data
+  // byte digitPins[] = {}; 
 
-  resistorsOnSegments = true; 
+  // bool resistorsOnSegments = false; 
+  // bool updateWithDelays = false;
+  // bool leadingZeros = false;
+  // bool disableDecPoint = true;
 
-  byte hardwareConfig = COMMON_CATHODE;
+  // byte hardwareConfig = COMMON_CATHODE;
 
-  // pinMode(latch, OUTPUT)
-  // pinMode(clock, OUTPUT)
-  // pinMode(data, OUTPUT)
 
-  sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments);
+  // // pinMode(latch, OUTPUT)
+  // // pinMode(clock, OUTPUT)
+  // // pinMode(data, OUTPUT)
 
-  pinMode(stop_button, INPUT_PULLUP);
+  // sevseg.begin(hardwareConfig, numDigits, digitPins, shiftRegisterPins, resistorsOnSegments, updateWithDelays, leadingZeros, disableDecPoint);
+
+  //  sevseg.setBrightness(90);
+
+  pinMode(data, OUTPUT);
+
+  pinMode(latch, OUTPUT);
+
+  pinMode(clock, OUTPUT);
+
+  pinMode(reset_button, INPUT_PULLUP);
 
   pinMode(start_button, INPUT_PULLUP);
-  
-  sevseg.setBrightness(90);
-  serial.Begin(9600);
 
 }
-
 
 void DisplayTime(){
   int displayValue = (minutes * 100) + seconds; 
-  sevseg.setNumber(displayValue, 2); 
+  
+  int digit[4] = 
+  {
+    (displayValue / 1000) % 10,
+    (displayValue / 100) % 10,
+    (displayValue / 10) % 10,
+    displayValue % 10
+
+  };
+
+  for (int i = 0; i < 4; i++){
+    shiftOut(data, clock, MSBFIRST, digitPatterns[digit[i]]);
+    shiftOut(data, clock, MSBFIRST, digitSelect[i]);
+
+    digitalWrite(latch, HIGH);
+    delay(5);
+    digitalWrite(latch, LOW);
+  }
 
 }
+
 
 void loop() {
   // put your main code here, to run repeatedly:
@@ -61,7 +109,7 @@ void loop() {
     isRunning = !isRunning; 
     
   }
-  if (digitalRead(stop_button) == LOW)
+  if (digitalRead(reset_button) == LOW)
   {
     delay(200);
     isRunning = false; 
@@ -70,13 +118,13 @@ void loop() {
 
   }
   if (isRunning){
-    unsigned long currentMillis = millis() 
+    unsigned long currentMillis = millis();
 
     if (currentMillis - previousMillis >= interval){
       previousMillis = currentMillis;
 
       if (minutes < 59 || seconds < 59){
-        seconds++
+        seconds++;
         if (seconds == 60){
           seconds = 0;
           minutes++;
@@ -90,10 +138,10 @@ void loop() {
 
   }
 
-  displayTime();
-  sevseg.refreshDisplay(); 
+  DisplayTime();
 
 }
+
 
 
 
